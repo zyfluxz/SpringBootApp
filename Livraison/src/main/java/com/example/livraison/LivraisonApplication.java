9c22f16e323b9105f16c31af10228f1178a9eba0
Unknown <ahmed.benghozzia@esprit.tn>
2019-11-13 21:20:56 +0100

    Livraison

---
+++ Livraison/src/main/java/com/example/livraison/LivraisonApplication.java
@@ -0,0 +1,13 @@
+package com.example.livraison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivraisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivraisonApplication.class, args);
+	}
+
+}
